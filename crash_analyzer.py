21ff75c495f0acc573bd195d38e6a10d24166e88
damoncai <damoncai@tencent.com>
2019-05-07 23:31:53 +0800

    add python script

---
+++ crash_analyzer.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*
+#!/usr/bin/python
+
+#日志扫描器，生成ResolvedInfo
+class LogScanner:
+	def scan(logFilePath):
+		return 2
+		
+
+# 解析后的crash信息
+class Context:
+	def test():
+		return
+
+class Rule:
+	def get():
+		return
+
+class Reporter:
+	def report_to(path):
+		return
+
+if __name__=="__main__":
+	print("===== crash analyze tool =====")
+
+	
